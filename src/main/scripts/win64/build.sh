7a601d0529fc1c43cdafa04f72c981724cb573fa
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-11-30 23:53:33 +0100

    Update build.sh

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,33 +1,14 @@
 #!/usr/bin/env bash
 
-#cp -R ../../c++ workdir
-#cd workdir
-#autoconf
-#./configure --host=x86_64-w64-mingw32
-#make
-#r1=$?
-#mkdir -p ../../../../../target/classes/win64/include
-#mkdir -p ../../../../../target/classes/win64/lib
-#cp Dist/FreeImage.h ../../../../../target/classes/win64/include
-#cp Dist/libfreeimage.a ../../../../../target/classes/win64/lib
-#cd ..
-#rm -r workdir
-
-#exit ${r1}
-
-#cmake ../../c++ \
-#-DCMAKE_BUILD_TYPE=Release \
-#-DCMAKE_INSTALL_PREFIX="../../../../target/classes/win64" \
-#-DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
-
 cd ../../c++
-./configure --prefix=../../target/classes/win64/
+./configure
 make
 
-ls -l
+make install
 
 r1=$?
 
-make clean
+cp /usr/local/lib/libcurl.a ../../../../target/classes/win64/lib
+cp -r /usr/local/include/curl ../../../../target/classes/win64/include
 
 exit ${r1}
