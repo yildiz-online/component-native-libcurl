78effc6fa86964d841b6a31cd075f496ec6a836a
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-11-30 07:38:30 +0100

    [WIP] Add build scripts.

---
+++ src/main/scripts/win64/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#cp -R ../../c++ workdir
+#cd workdir
+#autoconf
+#./configure --host=x86_64-w64-mingw32
+#make
+#r1=$?
+#mkdir -p ../../../../../target/classes/win64/include
+#mkdir -p ../../../../../target/classes/win64/lib
+#cp Dist/FreeImage.h ../../../../../target/classes/win64/include
+#cp Dist/libfreeimage.a ../../../../../target/classes/win64/lib
+#cd ..
+#rm -r workdir
+
+#exit ${r1}
+
+cmake ../../c++ \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX="../../../../target/classes/win64" \
+-DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+
+make install
+r1=$?
+
+make clean
+
+exit ${r1}
