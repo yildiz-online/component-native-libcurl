db91428def7c72fd7ca120c1122338ce010b9b90
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-01 16:43:08 +0100

    Update build.sh

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ../../c++
-./configure --disable-shared --enable-static
+./configure --disable-shared --enable-static --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32
 make
 
 make install
