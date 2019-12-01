f98a3c0e2ce8a08ebebde2f83521e3e87a3daebf
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-01 08:49:18 +0100

    Update build.sh

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -8,10 +8,10 @@ make install
 
 r1=$?
 
-mkdir ../../../target/classes/win64
-mkdir ../../../target/classes/win64/lib
-mkdir ../../../target/classes/win64/include
-cp /usr/local/lib/libcurl.a ../../../target/classes/win64/lib
-cp -r /usr/local/include/curl ../../../target/classes/win64/include
+mkdir /src/target/classes/win64
+mkdir /src/target/classes/win64/lib
+mkdir /src/target/classes/win64/include
+cp /usr/local/lib/libcurl.a /src/target/classes/win64/lib
+cp -r /usr/local/include/curl /src/target/classes/win64/include
 
 exit ${r1}
