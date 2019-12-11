01814589b2956cf659120eecdc7e0b8e9ce5e834
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-11 16:30:34 +0100

    Update build.sh

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export $ LDFLAGS="-L/src/src/main/scripts/win64/libidn2/win64/lib" CFLAGS="-I/src/src/main/scripts/win64/libidn2/win64/include"
+
 cd ../../c++
 ./configure --disable-shared --enable-static --disable-ldap --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32
 make
