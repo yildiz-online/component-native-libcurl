64f65004fc2acbf0b948aef63cd991aad0219cc9
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-12 09:30:17 +0100

    Update build.sh

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -2,6 +2,8 @@
 
 sed -i 's#/src/target/classes/win64/lib#/src/src/main/scripts/win64/libidn2/win64/lib#g' /src/src/main/scripts/win64/libidn2/win64/lib/libidn2.la
 
+export LDFLAGS="-L/src/src/main/scripts/win64/libidn2/win64/lib/" CPPFLAGS="-I/src/src/main/scripts/win64/libidn2/win64/include/"
+
 cd ../../c++
 ./configure --disable-shared --enable-static --disable-ldap --with-libidn2=/src/src/main/scripts/win64/libidn2/win64/ --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32
 make
